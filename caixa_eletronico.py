640b7d92093ecf256313e620cbeacd0b00a2abaf
gumeirinhos2401-hash <gumeirinhos2401@gmail.com>
2026-02-10 21:42:27 -0300

    Update caixa_eletronico.py

--- caixa_eletronico.py
+++ caixa_eletronico.py
@@ -1,3 +1,21 @@
+# Sistema de Caixa Eletrônico em Python
+
+Projeto desenvolvido com o objetivo de praticar lógica de programação e simular o funcionamento de um caixa eletrônico.
+
+## Funcionalidades
+- Consulta de saldo
+- Depósito
+- Saque
+- Histórico de transações
+- Menu interativo
+
+## Tecnologias
+- Python
+- Thonny
+
+## Autor
+Gustavo Meirinhos
+
 saldo = 1000
 historico = []
 opcao = ""
